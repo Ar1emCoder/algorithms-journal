0489132759e8260b0144a3b59a62c253feedb640
Artem Timkin <timkinartem84@gmail.com>
2026-08-21 23:04:23 +0300

    feat: add solutions for 0206-reverse-linked-list and 0021-merge-two-sorted-lists

---
+++ linked-lists/0206-reverse-linked-list.py
@@ -0,0 +1,25 @@
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        curr = head
+
+        while curr:
+            tmp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = tmp
+        return prev
+
+
+# Time Complexity: O(n) — проходим по списку один раз
+# Space Complexity: O(1) — используем только 3 переменные (prev, curr, tmp)
